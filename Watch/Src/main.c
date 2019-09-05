17716c2ba4fc43ee92f57bbd0585d416ea7e49f8
Sang Woo Yun <wwysw418@nate.com>
2019-09-05 19:14:30 +0900

    Add License

--- Watch/Src/main.c
+++ Watch/Src/main.c
@@ -42,6 +42,49 @@
 //volatile uint16_t adcval1;
 //volatile uint16_t adcval2;
 //volatile global variable
+
+
+// This file is a part of stm-cartcontroller
+// BSD 3-Clause License
+//
+// Copyright (c) 2019, Intelligent-distributed Cloud and Security Laboratory (ICNS Lab.)
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice, this
+//    list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+//    this list of conditions and the following disclaimer in the documentation
+//    and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the copyright holder nor the names of its
+//    contributors may be used to endorse or promote products derived from
+//    this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// title           : main.c
+// description     : C, stm-safety watch
+// author          : Bon Kwan Gu, Sang Woo Yoon
+// date            : 20190905
+// version         : 0.1
+// TrueSTUDIO	   : 9.3.0
+// notes           : This stm-safety watch is an implementation of a safe watch for
+//					 detecting electromagnetic wave in the C Programming Language.
+// ==============================================================================
+
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
